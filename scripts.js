825f938b992f17f49f3bb8ebd72300b0773da0ba
Emanuel Conrado <emanuelthadeu@gmail.com>
2023-07-11 18:03:24 -0300

    added function readAnime()

--- scripts.js
+++ scripts.js
@@ -1,55 +1,50 @@
-const fs =require('fs')
-const arq_anime = 'anime.txt'
-const path = 'anime.txt'
-
-function buttomClick(){
-    const input = document.querySelector(".search").value
-    searchAnime(input)
-}
-
-function lerArquivo(caminho){
-    fs.readFile(caminho, 'utf-8', function(error,data){
-        if(error){
-            console.log('readFile: Error')
-        }else{
-            const splitted = data.replaceAll('\n',',').split(',', 4)
-            const name = splitted[0]
-            const epsisodes = splitted[1]
-            const seasons = splitted[2]
-            const sinopse = splitted[3]
-            console.log(name, epsisodes, seasons, sinopse)
-
-            const anime = new Anime(name, epsisodes, seasons, sinopse)
-        }
-    })
-}
-
-function escreveArquivo(caminho, texto){
-    fs.writeFile(caminho, texto, function(error){
-        if(error){
-            console.error('writeFile: Error' + error.message)
-        }else{
-            console.log('writeFile: OK' + caminho)
-        }
-    })
-}
-
-function searchAnime(anime){
-
-    for(let i = 0; i < anime.length; i++){
-        if(anime[i] == anime){
-            console.log(anime[i])
-        }
-    }
-}
-
-lerArquivo(path)
+const fs =require('fs');
+const arq_anime = 'anime.txt';
+const path = 'anime.txt';
+let animes = [];
 
 class Anime{
     constructor(nome, epsisodes, seasons, sinopse){
-        this.nome = nome
-        this.epsisodes = epsisodes
-        this.seasons = seasons
-        this.sinopse = sinopse
+        this.nome = nome;
+        this.epsisodes = epsisodes;
+        this.seasons = seasons;
+        this.sinopse = sinopse;
+    }
+}
+
+function buttomClick(){
+    const input = document.querySelector(".search").value;
+    readArquivo();
+    searchAnime(input);
+    console.log(animes);
+}
+
+function readArquivo(){
+    try{
+        const data = fs.readFileSync(arq_anime, 'utf-8');
+        const lines = data.split('\n');
+        lines.forEach(line => {
+            const [nome, epsisodes, seasons, sinopse] = line.split(';');
+            const anime = new Anime(nome, epsisodes, seasons, sinopse);
+            animes.push(anime);
+        });
+    }catch(err){
+        console.log(err);
+        process.exit(-1);
+    }
+}
+
+function searchAnime(input){
+    const anime = animes.find(anime => anime.nome === input);
+    if(anime){
+        document.querySelector(".english-name").innerHTML = anime.nome;
+        document.querySelector(".epsisodes").innerHTML = anime.epsisodes;
+        document.querySelector(".seasons").innerHTML = anime.seasons;
+        document.querySelector(".sinopse").innerHTML = anime.sinopse;
+    }else{
+        document.querySelector(".anime").innerHTML = "Anime não encontrado";
+        document.querySelector(".epsisodes").innerHTML = "";
+        document.querySelector(".seasons").innerHTML = "";
+        document.querySelector(".sinopse").innerHTML = "";
     }
 }
